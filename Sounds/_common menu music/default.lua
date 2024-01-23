2cabf47b1bce66855b60a6fc66e44250bb0eab53
Lilly Walther <lilly@rehtlaw.me>
2024-01-23 07:53:58 +0100

    gtfo christmas theme

--- Sounds/_common menu music/default.lua
+++ Sounds/_common menu music/default.lua
@@ -29,8 +29,6 @@ if not file then file = songs.Hearts end
 if PREFSMAN:GetPreference("EasterEggs") and style ~= "Thonk" then
 	--  41 days remain until the end of the year.
 	if MonthOfYear()==10 and DayOfMonth()==20 then file = "20" end
-	-- the best way to spread holiday cheer is singing loud for all to hear
-	if MonthOfYear()==11 then file = "HolidayCheer" end
 end
 
 return THEME:GetPathS("", "_common menu music/" .. file)
