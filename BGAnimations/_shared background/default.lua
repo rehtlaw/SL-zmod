1490d098ea89bbcb1c3e293efd1d08f0343ff249
Lilly Walther <lilly@rehtlaw.me>
2024-03-10 12:38:02 +0100

    add other lua files as comments just in case

--- BGAnimations/_shared background/default.lua
+++ BGAnimations/_shared background/default.lua
@@ -1,28 +1,37 @@
-local af = Def.ActorFrame{}
+local af = Def.ActorFrame({})
 local randomBg = math.random(1, 4)
 
 -- a simple Quad to serve as the backdrop
-af[#af+1] = Def.Quad{
-	InitCommand=function(self) self:FullScreen():Center():diffuse( Color.Black ) end
-}
+af[#af + 1] = Def.Quad({
+	InitCommand = function(self)
+		self:FullScreen():Center():diffuse(Color.Black)
+	end,
+})
 
 -- add common background here
 --af[#af+1] = LoadActor("./wf07still.png")..{ -- If you are having performance issues, use this one instead
-af[#af+1] = LoadActor(string.format("./%s.mp4", randomBg))..{
-	InitCommand = function(self)
-		self:xy(_screen.cx, _screen.cy):zoomto(_screen.w, _screen.h):diffusealpha(0.75)
-	end
-}
+af[#af + 1] = LoadActor(string.format("./%s.mp4", randomBg))
+	.. {
+		InitCommand = function(self)
+			self:xy(_screen.cx, _screen.cy):zoomto(_screen.w, _screen.h):diffusealpha(0.75)
+		end,
+	}
 
-af[#af+1] = Def.Quad{
+af[#af + 1] = Def.Quad({
 	ScreenChangedMessageCommand = function(self)
 		self:visible(SCREENMAN:GetTopScreen():GetName() ~= "ScreenTitleMenu")
 	end,
 	InitCommand = function(self)
 		self:zoom(SCREEN_WIDTH, SCREEN_HEIGHT)
 		self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y)
-		self:diffuse(0,0,0,0.4)
-	end
-}
+		self:diffuse(0, 0, 0, 0.4)
+	end,
+})
+
+-- af[#af+1] = LoadActor("./Normal.lua", file)
+-- af[#af+1] = LoadActor("./RainbowMode.lua", file)
+--
+-- af[#af+1] = LoadActor("./Static.lua", file)
+-- af[#af+1] = LoadActor("./Technique.lua", file)
 
 return af
