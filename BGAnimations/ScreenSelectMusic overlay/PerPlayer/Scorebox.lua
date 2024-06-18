440c6b583ee5fc00b3beee76e8f46b7b325b83af
Lilly Walther <lilly@rehtlaw.me>
2024-06-18 08:50:33 +0200

    fucked up the merge lol

--- BGAnimations/ScreenSelectMusic overlay/PerPlayer/Scorebox.lua
+++ BGAnimations/ScreenSelectMusic overlay/PerPlayer/Scorebox.lua
@@ -729,10 +729,10 @@ local af = Def.ActorFrame({
 		end,
 	}),
 	-- SRPG Logo
-	Def.Sprite{
-		Texture=THEME:GetPathG("", "rpg8 (doubleres).png"),
-		Name="SRPG8Logo",
-		InitCommand=function(self)
+	Def.Sprite({
+		Texture = THEME:GetPathG("", "rpg8 (doubleres).png"),
+		Name = "SRPG8Logo",
+		InitCommand = function(self)
 			self:diffusealpha(0.4):zoom(0.3):diffusealpha(0)
 		end,
 		LoopScoreboxCommand = function(self)
